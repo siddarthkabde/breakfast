dea7eb8c084fc34c7cd3a047c7ac1c48cc64cd1f
siddarthkabde <siddarth.kabde@gmail.com>
2020-02-26 14:10:00 +0000

    V7

--- webapp/controller/Profile.controller.js
+++ webapp/controller/Profile.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
 	"./BaseController"
-], function(BaseController) {
+], function (BaseController) {
 	"use strict";
 
 	return BaseController.extend("ZIProfile.controller.Profile", {
@@ -10,39 +10,77 @@ sap.ui.define([
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf ZIProfile.view.Profile
 		 */
-			onInit: function() {
-				// var imgLinkedin,imgProfile;
-				// imgLinkedin = this.getView().byId("imgLinkedin");
-				// imgProfile = sap.ui.getCore().byId("__content0");
-				// imgLinkedin.addStyleClass("imgLinkedin");
-				// imgProfile.addStyleClass("imgProfile");
+		onInit: function () {
+			// var imgLinkedin,imgProfile;
+			// imgLinkedin = this.getView().byId("imgLinkedin");
+			// imgProfile = sap.ui.getCore().byId("__content0");
+			// imgLinkedin.addStyleClass("imgLinkedin");
+			// imgProfile.addStyleClass("imgProfile");
+		},
+
+		handleUploadPress: function () {
+			var oEntry1 = this.mapoEntry1();
+		},
+
+		// function to upload the data form the file into the table.
+		mapoEntry1: function () {
+			// var oGlobalBusyDialog = new sap.m.BusyDialog();
+			// oGlobalBusyDialog.open();
+			var t = this;
+			var oFileUploader = this.getView().byId("fileUploader");
+			if (!oFileUploader.getValue()) {
+				MessageToast.show("choose a file first");
+			} else {
+				var oTable = this.getView().byId("idTable");
+				var file = oFileUploader.getFocusDomRef().files[0];
+				if (file && window.FileReader) {
+					var reader = new FileReader();
+					reader.onload = function (e) {
+						var that = this;
+						var data = e.target.result;
+						var workbook = XLSX.read(data, {
+							type: 'binary'
+						});
+
+						workbook.SheetNames.forEach(function (sheetName) {
+							var that = this;
+							if (sheetName != 'Student Master') {
+								return;
+							}
+
+							var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+							var finalArr = XL_row_object.filter(function (val) {
+								// return val.Agrmnt No. != "";
+								if (!val.Name) {
+									return;
+								}
+								if (val.Adm) {
+									val.Adm = val.Adm.toString();
+								}
+								if (val.Cell) {
+									val.Cell = val.Cell.toString();
+								}
+
+							});
+
+							oModel.setData(XL_row_object);
+							// var oStore = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+							// // //storage
+							// oStore.put("studentModel", XL_row_object);
+							// sap.ui.getCore().setModel(oModel, "studentsModel");
+
+							// var oStore1 = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+							// var oDate = oStore1.get("studentData");
+
+						});
+					};
+
+					reader.readAsBinaryString(file);
+
+				}
 			}
 
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf ZIProfile.view.Profile
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf ZIProfile.view.Profile
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf ZIProfile.view.Profile
-		 */
-		//	onExit: function() {
-		//
-		//	}
+		},
 
 	});
 
